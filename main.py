d5a15f160a9ddd7c1f9ac02fde3f8b4674c71688
Muhammadrasul <holturaevm@gmail.com>
2023-07-06 23:03:01 +0300

    variables in python

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-#
-# last_name = 'Kholturaev'
-# first_name = 'Muhammadrasul'
-# full_name = first_name + " " + last_name
-# print('Hello, ' + full_name)
-#
-# # print(type(name))
+# VARIABLES IN PYTHON
+# String
+
+last_name = 'Kholturaev'
+first_name = 'Muhammadrasul'
+full_name = first_name + " " + last_name
+print('Hello, ' + full_name)
+# print(type(name))
+
+
+
+# Number
 
 age = 20
 age += 2
 print('Your age is ' + str(age))
 # print(type(age))
+
+
+
+# Float
+
+height = 250.5
+print('Your height is: ' + str(height) + 'cm')
+# print(type(height))
+
+
+
+
+# Boolean variables
+human = True
+print("Are you a human: " + str(human))
+# print(type(human))
+
