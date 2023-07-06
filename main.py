00a9992b8235a9e31a591139a223122fc1a7cce1
Muhammadrasul <holturaevm@gmail.com>
2023-07-06 22:52:29 +0300

    started learning pythod, first lines of py code

---
+++ main.py
@@ -0,0 +1,12 @@
+#
+# last_name = 'Kholturaev'
+# first_name = 'Muhammadrasul'
+# full_name = first_name + " " + last_name
+# print('Hello, ' + full_name)
+#
+# # print(type(name))
+
+age = 20
+age += 2
+print('Your age is ' + str(age))
+# print(type(age))
