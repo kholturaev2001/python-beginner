5f1a53cf1d7f4a04f382be44b9f0454f5a60e472
Muhammadrasul <holturaevm@gmail.com>
2023-07-08 11:19:57 +0300

    nested loops

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-import time
+# nested loops    =   one loop inside of another loop, i.e. The "inner loop" will finish all of its iterations
+#                     before finishing one interation of the "outer loop"
 
-# for loop = a statement that will execute its block of code a limited amount of times
+rows = int(input("How many rows?: "))
+columns = int(input("How many columns?: "))
+symbol = input('Enter a symbol to use: ')
 
-
-#                               while loop = unlimited
-#                               for loop = limited
-
-# for i in range(10):
-#     print(i+1)
-
-# for i in range(50, 100+1, 2):
-#     print(i)
-
-# for i in 'Bro Code':
-#     print(i)
-
-
-for seconds in range(10, 0, -1):
-    print(seconds)
-    time.sleep(1)
-print('Happy New Year!')
+for i in range(rows):
+    for j in range(columns):
+        print(symbol, end='')
+    print()
