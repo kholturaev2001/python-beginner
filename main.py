75a4857db5d296d96af0c1580161a582c39b4291
Muhammadrasul <holturaevm@gmail.com>
2023-07-08 11:42:05 +0300

    list = used to store a multiple items in a single variable  => array

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
-# Loop Control Statements = change a loops execution from its normal sequence
+# list = used to store a multiple items in a single variable  => array
 
-# break =           used to terminate the loop entirely
-# continue =        skips to the next interation of the loop
-# pass =            does nothing, acts as a placeholder
+food = ['pizza', 'hamburger', 'osh', 'mantu', 'pudding']
 
-# while True:
-#     name = input('Enter your name: ')
-#     if name.strip() != "":
-#         break
+food[0] = 'sushi'
 
+# food.append('ice cream')
+# food.remove('hamburger')
+# food.pop()
+# food.insert(1, 'cake')
+# food.sort()
+# food.clear()
 
-
-#
-# phone_number = '123-456-7890'
-#
-# for i in phone_number:
-#     if i == '-':
-#         continue
-#     print(i, end='')
-
-for i in range(1, 21):
-    if i == 13:
-        pass
-    else:
-        print(i)
+for x in food:
+    print(x)
