d0de39867165077395f5fde915b8d70fe5100ef2
Muhammadrasul <holturaevm@gmail.com>
2023-07-11 12:17:44 +0300

    str.format() =    optional method that gives users more control when displaying output

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-# **kwargs =    parameters that will pack all arguments into a dictionary
-#               useful so that a function can accept a varying amount of keyword arguments
 
-def hello(**kwargs):
-    # print('Hello ' + kwargs['first'] + ' ' + kwargs['last'])
-    print('Hello', end=' ')
-    for key, value in kwargs.items():
-        print(value, end=' ')
+# str.format() =    optional method that gives users more control when displaying output
 
-hello(title='Mr.', first='Muhammadrasul', last='Kholturaev', middle='Abdulazizovich')
+animal = 'cow'
+item = 'moon'
+
+# print('The ' + animal + ' jumped over the ' + item)
+# print("The {} jumped over the {}".format(animal, item))
+# print("The {1} jumped over the {0}".format(animal, item)) # positional argument
+# print("The {animal} jumped over the {animal}".format(animal='cow', item='moon')) # keyword argument
+
+text = 'The {} jumped over the {}'
+print(text.format(animal, item))
