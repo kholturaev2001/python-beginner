2bf077a2466ace01012ce3aafeec088b23225e43
Muhammadrasul <holturaevm@gmail.com>
2023-07-15 11:58:10 +0300

    get modules help

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-# module = a file containing python code. May contain function, classes, etc.
-# used with modular programming, which is to separate a program into parts
-
-# import messages as msg
-# msg.hello()
-# msg.bye()
-
-from messages import hello, bye # or from messages import *
-hello()
-bye()
+help('modules email')
