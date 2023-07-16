1f60ee03e73290ac2f1928d34685cabcb5c03400
Muhammadrasul <holturaevm@gmail.com>
2023-07-17 01:55:45 +0300

    multi-level inheritance = when a derived (child) class inherits another derived (child) class

--- main.py
+++ main.py
@@ -1,36 +1,20 @@
-# INHERITANCE in python
-# Classes can have children.
+# multi-level inheritance = when a derived (child) class inherits another derived (child) class
 
-class Animal: # parent class
+class Organism:
 
     alive = True
+
+class Animal(Organism):
 
     def eat(self):
         print('This animal is eating')
 
-    def sleep(self):
-        print('This animal is sleeping')
+class Dog(Animal):
 
+    def bark(self):
+        print('This dog is barking')
 
-class Rabbit(Animal): # this is a child class, and it inherits all what the Animal class has
-    def run(self):
-        print('This rabbit is running')
-class Fish(Animal):
-    def swim(self):
-        print('This fish is swimming')
-class Hawk(Animal):
-    def fly(self):
-        print('This hawk is flying')
-
-rabbit = Rabbit()
-fish = Fish()
-hawk = Hawk()
-
-# print(rabbit.alive)
-# fish.eat()
-# hawk.sleep()
-
-rabbit.run()
-fish.swim()
-hawk.fly()
-
+dog = Dog()
+print(dog.alive)
+dog.eat()
+dog.bark()
